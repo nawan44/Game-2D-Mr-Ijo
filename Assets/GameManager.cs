fcf13065e5ee90ef26a8ff9b201e7563f4824dca
nawan44 <rachmat.d.gunawan@gmail.com>
2020-05-16 21:49:13 +0700

    started

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager gm;
+
+	public int score;
+
+	void Awake(){
+		gm = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
